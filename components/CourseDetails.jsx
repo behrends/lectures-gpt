907e811924223e7123b43b256652d0086caf2411
Erik Behrends <behrends@users.noreply.github.com>
2023-05-15 15:38:15 +0200

    Extract translations into an object and add French

--- components/CourseDetails.jsx
+++ components/CourseDetails.jsx
@@ -1,44 +1,23 @@
-const CourseDetails = ({ course, language }) => {
+function CourseDetails({ course, translation }) {
   if (!course) {
-    return (
-      <div>
-        <p className="text-xl font-semibold">
-          {language === 'English'
-            ? 'Please select a course'
-            : 'Bitte wählen Sie einen Kurs'}
-        </p>
-      </div>
-    );
+    return <p>{translation.selectPrompt}</p>;
   }
 
   return (
-    <div>
-      <h2 className="text-2xl font-semibold">{course.name}</h2>
+    <div className="p-4 border border-gray-300 rounded-md">
+      <h2 className="text-xl font-semibold mb-2">{course.name}</h2>
+      <p>
+        {translation.instructor}: {course.instructor}
+      </p>
+      <p>
+        {translation.credits}: {course.credits}
+      </p>
+      <p>
+        {translation.prerequisites}: {course.prerequisites.join(', ')}
+      </p>
       <p>{course.description}</p>
-      <p>
-        {language === 'English' ? 'Instructor:' : 'Dozent:'}{' '}
-        {course.instructor}
-      </p>
-      <p>
-        {language === 'English' ? 'Credits:' : 'Credits:'}{' '}
-        {course.credits}
-      </p>
-      {course.prerequisites.length > 0 && (
-        <>
-          <p>
-            {language === 'English'
-              ? 'Prerequisites:'
-              : 'Voraussetzungen:'}
-          </p>
-          <ul>
-            {course.prerequisites.map((prerequisite, index) => (
-              <li key={index}>{prerequisite}</li>
-            ))}
-          </ul>
-        </>
-      )}
     </div>
   );
-};
+}
 
 export default CourseDetails;
