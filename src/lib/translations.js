3c8d9318a454f14c240efbd6a96c0e8e76580a53
Erik Behrends <behrends@users.noreply.github.com>
2023-05-16 20:14:29 +0200

    Extract data and translations

---
+++ src/lib/translations.js
@@ -0,0 +1,30 @@
+const translations = {
+  en: {
+    title: 'Computer Science Lectures',
+    searchPlaceholder: 'Search lectures...',
+    instructor: 'Instructor',
+    credits: 'Credits',
+    prerequisites: 'Prerequisites',
+    selectPrompt: 'Select a lecture to see its details.',
+  },
+  de: {
+    title: 'Vorlesungen in Informatik',
+    searchPlaceholder: 'Vorlesungen durchsuchen...',
+    instructor: 'Dozent',
+    credits: 'Kredits',
+    prerequisites: 'Voraussetzungen',
+    selectPrompt:
+      'Wählen Sie eine Vorlesung aus, um Details zu sehen.',
+  },
+  fr: {
+    title: 'Conférences en informatique',
+    searchPlaceholder: 'Chercher des conférences...',
+    instructor: 'Instructeur',
+    credits: 'Crédits',
+    prerequisites: 'Prérequis',
+    selectPrompt:
+      'Sélectionnez une conférence pour voir ses détails.',
+  },
+};
+
+export default translations;
